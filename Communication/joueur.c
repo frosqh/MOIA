267e034354b5c91b17789199962fc1f825c11b52
DjMerah <djmerah360@gmail.com>
2019-03-26 12:27:10 +0100

    fichiers de communication c

---
+++ Communication/joueur.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <errno.h>
+#include "fonctionsTCP.h"
+#include "protocolYokai.h"
+
+
+
+  
+
+  
